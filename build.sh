5de7572982b3e88747f6637cfb8c4f18724e7d8d
sycured <60801403+sycured@users.noreply.github.com>
2021-03-14 12:41:27 -0500

    add build script to build OCI + workflow to do it

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+mkimg=$(buildah from oraclelinux:8-slim)
+buildah config --author='sycured' "$mkimg"
+buildah config --label Name='streaming_calc_actixweb' "$mkimg"
+buildah config --env APP_IP=0.0.0.0 "$mkimg"
+buildah config --env APP_PORT=8080 "$mkimg"
+buildah config --port 8080 "$mkimg"
+buildah config --workingdir='/opt' "$mkimg"
+buildah config --cmd '/opt/streaming_calc_actixweb' "$mkimg"
+buildah copy "$mkimg" 'target/release/streaming_calc_actixweb' '/opt/streaming_calc_actixweb'
+buildah commit --squash "$mkimg" "scaw"
+buildah rm "$mkimg"
