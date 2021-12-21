0bcb846c7ea7135fc8d7f44b1725e71d39811c9c
sycured <60801403+sycured@users.noreply.github.com>
2021-12-21 08:50:30 -0500

    fix: this expression borrows a reference that is immediately dereferenced by the compiler

--- src/trait_imp.rs
+++ src/trait_imp.rs
@@ -8,11 +8,11 @@ impl BodyTest for ResponseBody<Body> {
     fn as_str(&self) -> &str {
         match self {
             ResponseBody::Body(ref b) => match b {
-                Body::Bytes(ref by) => std::str::from_utf8(&by).unwrap(),
+                Body::Bytes(ref by) => std::str::from_utf8(by).unwrap(),
                 _ => panic!(),
             },
             ResponseBody::Other(ref b) => match b {
-                Body::Bytes(ref by) => std::str::from_utf8(&by).unwrap(),
+                Body::Bytes(ref by) => std::str::from_utf8(by).unwrap(),
                 _ => panic!(),
             },
         }
