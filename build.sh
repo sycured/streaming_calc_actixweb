ebd5e5a5e49a427a85d00ee3767ad33edb8d5648
sycured <60801403+sycured@users.noreply.github.com>
2022-01-12 21:34:56 -0500

    move to static build

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -xe
 
-mkimg=$(buildah from ubuntu:20.04)
+mkimg=$(buildah from scratch)
 buildah config --author='sycured' "$mkimg"
 buildah config --label Name='streaming_calc_actixweb' --label org.opencontainers.image.source="https://github.com/sycured/streaming_calc_actixweb" "$mkimg"
 buildah config --env APP_IP=0.0.0.0 "$mkimg"
 buildah config --env APP_PORT=8080 "$mkimg"
 buildah config --port 8080 "$mkimg"
-buildah config --workingdir='/opt' "$mkimg"
-buildah config --cmd '/opt/streaming_calc_actixweb' "$mkimg"
-buildah copy "$mkimg" 'target/release/streaming_calc_actixweb' '/opt/streaming_calc_actixweb'
-buildah copy "$mkimg" 'static' '/opt/static/'
+buildah config --cmd '/streaming_calc_actixweb' "$mkimg"
+buildah copy "$mkimg" 'target/x86_64-unknown-linux-gnu/release/streaming_calc_actixweb' '/streaming_calc_actixweb'
+buildah copy "$mkimg" 'static' '/static/'
 buildah commit --squash "$mkimg" "scaw"
 buildah rm "$mkimg"
