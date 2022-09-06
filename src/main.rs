bc85827a2d4ad701994e9e8b27acab4ff63fc768
sycured <60801403+sycured@users.noreply.github.com>
2022-09-06 11:15:09 -0500

    add jemalloc + codegen-units=1 for release

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 use actix_cors::Cors;
 use actix_web::{
     middleware::{Compress, Logger},
