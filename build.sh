102f0e5d9a70eff97c0129d5075057be9bdab4b1
sycured <60801403+sycured@users.noreply.github.com>
2022-11-19 01:55:46 -0500

    add arch to the build

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -xe
 
 mkimg=$(buildah from scratch)
+buildah config --arch amd64 "$mkimg"
 buildah config --author='sycured' "$mkimg"
 buildah config --label Name='streaming_calc_actixweb' --label org.opencontainers.image.source="https://github.com/sycured/streaming_calc_actixweb" "$mkimg"
 buildah config --env APP_IP=0.0.0.0 "$mkimg"
