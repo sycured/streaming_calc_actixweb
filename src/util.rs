1f1d26106618dd0e2dc11eaf21c9ba126543134c
sycured <60801403+sycured@users.noreply.github.com>
2022-12-19 09:56:47 -0500

    add tests for util.rs

--- src/util.rs
+++ src/util.rs
@@ -31,3 +31,55 @@ pub fn return_response_plain(body: &'static str, x_request_id: Option<&str>) -> 
     }
     response
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::to_bytes, test::TestRequest};
+    use serde_json::json;
+
+    use super::{get_x_request_id_header, return_response_json, return_response_plain};
+
+    #[actix_web::test]
+    async fn test_get_x_request_id_header() {
+        let x_request_id = "4D590945-64E8-45F8-B34F-737608FAB875";
+        let req = TestRequest::default()
+            .insert_header(("x-request-id", x_request_id))
+            .to_http_request();
+        assert_eq!(get_x_request_id_header(&req).unwrap(), x_request_id);
+    }
+
+    #[actix_web::test]
+    async fn test_return_response_json() {
+        let x_request_id = "4D590945-64E8-45F8-B34F-737608FAB875";
+        let payload = json!({
+            "code": 200,
+            "success": true,
+            "payload": {
+                "features": [
+                    "serde",
+                    "json"
+                ]
+            }
+        });
+
+        let resp = return_response_json(payload.clone(), Some(x_request_id));
+
+        assert_eq!(resp.headers().get("x-request-id").unwrap(), x_request_id);
+
+        assert_eq!(
+            to_bytes(resp.into_body()).await.unwrap(),
+            payload.to_string().as_bytes()
+        );
+    }
+
+    #[actix_web::test]
+    async fn test_resturn_response_plain() {
+        let x_request_id = "4D590945-64E8-45F8-B34F-737608FAB875";
+        let body = &"It's a test";
+
+        let resp = return_response_plain(body, Some(x_request_id));
+
+        assert_eq!(resp.headers().get("x-request-id").unwrap(), x_request_id);
+        assert_eq!(to_bytes(resp.into_body()).await.unwrap(), body);
+    }
+}
