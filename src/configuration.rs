9adf7fda02c20bca89fe0b5f855735c98cfbb7c7
sycured <60801403+sycured@users.noreply.github.com>
2024-01-04 23:43:49 -0500

    fix lint

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,7 +5,6 @@ use log::warn;
 
 pub async fn get_from_env_or_default(env_name: &str, default: String) -> String {
     var(env_name).unwrap_or_else(|_| {
-        let default = default;
         warn!(
             "Using default {name}: {value}",
             name = env_name,
